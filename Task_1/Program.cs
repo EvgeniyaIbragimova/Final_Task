7dcdfc02a8eabf8771f07f3e5926c526e73f4dd3
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-11-27 15:03:19 +0300

    added solution

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -10,3 +10,32 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array1 = new string[5] { "Hello", "2", "world", ":-)", "123" };
+string[] array2 = new string[array1.Length];
+
+void ArrayThreeElements(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array1);
+ArrayThreeElements(array1, array2);
+PrintArray(array2);
